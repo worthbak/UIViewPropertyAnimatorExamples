e6620b2ce16f4cc2834c226f813b259462f296e1
Worth Baker <wbaker@housecanary.com>
2017-06-20 10:29:48 -0600

    Initial Commit

---
+++ PropertyAnimatorTests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PropertyAnimatorTests
+//
+//  Created by Worth Baker on 6/20/17.
+//  Copyright © 2017 HouseCanary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
