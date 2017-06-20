91eb5278b373cf96d161a4aaec08b157374d0da6
Worth Baker <wbaker@housecanary.com>
2017-06-20 14:54:33 -0600

    bouncy center animation

--- PropertyAnimatorTests/ViewController.swift
+++ PropertyAnimatorTests/ViewController.swift
@@ -10,66 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let normalDuration: TimeInterval = 4
-    lazy var circleAnimator: UIViewPropertyAnimator = {
-        return self.createAnimation()
-    }()
+    var circleCenter = CGPoint.zero
     
-    lazy var animatingView: UIView = {
-        let av = UIView(frame: .zero)
-        av.backgroundColor = .red
-        
-        return av
-    }()
+    var circleAnimator: UIViewPropertyAnimator?
     
-    var viewCenter = CGPoint.zero
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        animatingView.frame = CGRect(x: 24, y: view.bounds.height / 2.0, width: 150, height: 150)
-        view.addSubview(animatingView)
+        let circle = UIView(frame: CGRect(x: 0, y: 0, width: 150, height: 150))
+        circle.center = view.center
+        circle.layer.cornerRadius = 75
+        circle.backgroundColor = .green
         
-        animatingView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(didPan)))
+        circle.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(dragCircle)))
+        view.addSubview(circle)
     }
     
-    func createAnimation() -> UIViewPropertyAnimator {
-        return UIViewPropertyAnimator(duration: normalDuration, curve: .easeInOut, animations: {
-            self.animatingView.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
-        })
-    }
-    
-    func didPan(gesture: UIPanGestureRecognizer) {
+    func dragCircle(gesture: UIPanGestureRecognizer) {
         guard let target = gesture.view else { return }
         
         switch gesture.state {
-        case .began, .ended:
-            viewCenter = target.center
-            
-            let durationFactor = circleAnimator.fractionComplete
-            
-            // reset to inactive state
-            circleAnimator.stopAnimation(true)
-            
-            if gesture.state == .began {
-                circleAnimator.addAnimations {
-                    target.backgroundColor = .blue
-                    target.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
-                }
-            } else {
-                circleAnimator.addAnimations {
-                    target.backgroundColor = .red
-                    target.transform = CGAffineTransform.identity
-                }
+        case .began:
+            if let animator = circleAnimator, animator.isRunning {
+                animator.stopAnimation(false)
             }
             
-            circleAnimator.startAnimation()
-            circleAnimator.pauseAnimation()
-            circleAnimator.continueAnimation(withTimingParameters: nil, durationFactor: durationFactor)
+            circleCenter = target.center
         case .changed:
-            let trans = gesture.translation(in: self.view)
-            target.center = CGPoint(x: viewCenter.x + trans.x, y: viewCenter.y + trans.y)
+            let translation = gesture.translation(in: view)
+            target.center = CGPoint(x: circleCenter.x + translation.x, y: circleCenter.y + translation.y)
+        case .ended:
+            let velocity = gesture.velocity(in: target)
+            let vector = CGVector(dx: velocity.x / 500, dy: velocity.y / 500)
+            let params = UISpringTimingParameters(mass: 2.5, stiffness: 70, damping: 55, initialVelocity: vector)
+            circleAnimator = UIViewPropertyAnimator(duration: 0, timingParameters: params)
+            
+            circleAnimator?.addAnimations {
+                target.center = self.view.center
+            }
+            
+            circleAnimator?.startAnimation()
         default:
             break
         }
