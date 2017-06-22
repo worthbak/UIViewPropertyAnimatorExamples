be1f7737299dbdb38b7ffb16c8aff28404e37301
Worth Baker <wbaker@housecanary.com>
2017-06-22 11:05:48 -0600

    finalized drawer interaction

--- PropertyAnimatorTests/ViewController.swift
+++ PropertyAnimatorTests/ViewController.swift
@@ -8,46 +8,114 @@
 
 import UIKit
 
+enum State {
+    case expanded, minimized
+}
+
 class ViewController: UIViewController {
     
-    var circleCenter = CGPoint.zero
+    var runningAnimators = [Int: UIViewPropertyAnimator]()
+    var progressWhenInterrupted: CGFloat = 0
     
-    var circleAnimator: UIViewPropertyAnimator?
+    lazy var width: CGFloat = { return self.view.frame.width - 8 }()
+    lazy var topFrame: CGRect = { return CGRect(x: 4, y: 100, width: self.width, height: self.view.frame.height) }()
+    lazy var bottomFrame: CGRect = { return CGRect(x: 4, y: self.view.frame.height - 100, width: self.width, height: self.view.frame.height) }()
+    lazy var totalVerticalDistance: CGFloat = { self.bottomFrame.minY - self.topFrame.minY }()
+    
+    var viewState: State = .minimized
+    lazy var bottomView: UIView = {
+        let bv = UIView(frame: .zero)
+        bv.backgroundColor = .red
+        bv.layer.cornerRadius = 4
+        
+        return bv
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let circle = UIView(frame: CGRect(x: 0, y: 0, width: 150, height: 150))
-        circle.center = view.center
-        circle.layer.cornerRadius = 75
-        circle.backgroundColor = .green
+        bottomView.frame = bottomFrame
+        view.addSubview(bottomView)
         
-        circle.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(dragCircle)))
-        view.addSubview(circle)
-        
-        circleAnimator = UIViewPropertyAnimator(duration: 1.0, curve: .linear, animations: { 
-            circle.transform = CGAffineTransform(scaleX: 3.0, y: 3.0)
-        })
-        
-        circleAnimator?.addAnimations({
-            circle.backgroundColor = UIColor.blue
-        }, delayFactor: 0.75)
+        bottomView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(bottomViewTapped)))
+        bottomView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(bottomViewPanned)))
     }
     
-    func dragCircle(gesture: UIPanGestureRecognizer) {
-        guard let target = gesture.view else { return }
+    func animateTransitionIfNeeded(state: State, duration: TimeInterval) {
+        if runningAnimators.isEmpty {
+            let frameAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1) {
+                switch state {
+                case .minimized:
+                    self.bottomView.frame = self.topFrame
+                case .expanded:
+                    self.bottomView.frame = self.bottomFrame
+                }
+            }
+            
+            let identifier = frameAnimator.hash
+            frameAnimator.addCompletion { position in
+                self.cleanup(animatorWithId: identifier, at: position)
+            }
+            
+            frameAnimator.startAnimation()
+            runningAnimators[identifier] = frameAnimator
+        }
+    }
+    
+    func bottomViewPanned(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: bottomView)
+        let verticalTranslation = viewState == .minimized ? -translation.y : translation.y
+        let fraction = (verticalTranslation / totalVerticalDistance) + progressWhenInterrupted
         
         switch gesture.state {
         case .began:
-            circleCenter = target.center
-        case .changed:
-            let translation = gesture.translation(in: view)
-            target.center = CGPoint(x: circleCenter.x + translation.x, y: circleCenter.y + translation.y)
+            animateTransitionIfNeeded(state: viewState, duration: 0.5)
             
-            circleAnimator?.fractionComplete = target.center.y / view.frame.height
+            runningAnimators.forEach { $1.pauseAnimation() }
+            progressWhenInterrupted = runningAnimators.first?.value.fractionComplete ?? 0
+        case .changed:
+            runningAnimators.forEach { $1.fractionComplete = fraction }
+        case .ended:
+            let velocity = gesture.velocity(in: bottomView)
+            
+            switch viewState {
+            case .minimized:
+                if velocity.y > -500 && fraction < 0.5 {
+                    runningAnimators.forEach { $1.isReversed = !$1.isReversed }
+                }
+            case .expanded:
+                if velocity.y < 500 && fraction < 0.5 {
+                    runningAnimators.forEach { $1.isReversed = !$1.isReversed }
+                }
+            }
+            
+            runningAnimators.forEach { $1.continueAnimation(withTimingParameters: nil, durationFactor: 1) }
         default:
             break
         }
+    }
+    
+    func bottomViewTapped(gesture: UITapGestureRecognizer) {
+        if runningAnimators.isEmpty {
+            animateTransitionIfNeeded(state: viewState, duration: 0.5)
+        } else {
+            runningAnimators.forEach { $1.isReversed = !$1.isReversed }
+        }
+    }
+    
+    func cleanup(animatorWithId identifier: Int, at position: UIViewAnimatingPosition) {
+        if position == .end {
+            switch self.bottomView.frame {
+            case self.bottomFrame:
+                self.viewState = .minimized
+            case self.topFrame:
+                self.viewState = .expanded
+            default:
+                break
+            }
+        }
+        
+        self.runningAnimators.removeValue(forKey: identifier)
     }
 
 }
